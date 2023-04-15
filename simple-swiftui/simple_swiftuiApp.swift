75eabe565f21a0f4fe72a3dd9ea155762fd57f65
afitra <loeby45@gmail.com>
2023-04-15 19:13:40 +0700

    Initial Commit

---
+++ simple-swiftui/simple_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_swiftuiApp.swift
+//  simple-swiftui
+//
+//  Created by afitra mamor on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct simple_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
